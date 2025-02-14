2054d107fc441e0417278cab8eb22112cd71466d
Tien Thanh <nguyenthanh2542001@gmail.com>
2025-02-14 16:56:49 +0700

    Format response before sending

---
+++ src/main/java/vn/bookstoreProject/bookstore/util/FormatRestResponse.java
@@ -0,0 +1,52 @@
+package vn.bookstoreProject.bookstore.util;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import jakarta.servlet.http.HttpServletResponse;
+import vn.bookstoreProject.bookstore.domain.response.RestResponse;
+
+@ControllerAdvice
+public class FormatRestResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+            Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+            ServerHttpResponse response) {
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(status);
+
+        // Handle Exception: RestResponse cannot be cast to class String
+        if (body instanceof String || body instanceof Resource) {
+            return body;
+        }
+
+        // case error
+        if (status >= 400) {
+            return body;
+        } else {
+            // case success
+            res.setData(body);
+            res.setMessage("Call api success");
+        }
+
+        return res;
+    }
+
+    // https://stackoverflow.com/questions/24072458/how-can-i-wrap-a-json-response-in-spring/52104852#52104852
+
+}
